5a8ab0bd6d15c87ab86fccdbf21bb777f596e753
s-omas <49887549+s-omas@users.noreply.github.com>
2020-07-31 12:34:28 -0600

    add mechanism pages, fix styling

--- interactive/mechanism/views.py
+++ interactive/mechanism/views.py
@@ -3,4 +3,14 @@ from django.shortcuts import render
 
 def mechanism(request):
     context = {}
-    return render(request, 'mechanism.html', context)
+    return render(request, 'mechanism/reactions.html', context)
+
+
+def molecules(request):
+    context = {}
+    return render(request, 'mechanism/molecules.html', context)
+
+
+def reactions(request):
+    context = {}
+    return render(request, 'mechanism/reactions.html', context)
