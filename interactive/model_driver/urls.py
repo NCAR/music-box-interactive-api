6b01720a9adde473099edf5cb7d0717e1cf73792
s-omas <49887549+s-omas@users.noreply.github.com>
2020-08-05 14:46:21 -0600

    url fix

--- interactive/model_driver/urls.py
+++ interactive/model_driver/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('run', views.run, name='run'),
+    path('configure/model/run', views.run),
     path('check_status', views.check_status, name="check_status"),
     path('mechanism_data', views.mechanism_data, name="mechanism_data"),
     path('download', views.download, name="download"),
