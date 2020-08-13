7d73bceac669dabc7150bd8c0109632aad6e1bad
s-omas <49887549+s-omas@users.noreply.github.com>
2020-08-13 15:26:48 -0600

    html

--- interactive/dashboard/forms/optionsforms.py
+++ interactive/dashboard/forms/optionsforms.py
@@ -6,7 +6,7 @@ class OptionsForm(forms.Form):
     def __init__(self, *args, **kwargs):
         super(OptionsForm, self).__init__(*args, **kwargs)
         inits = option_setup()
-        self.fields['grid'] = forms.ChoiceField(choices=[('box', 'Box'), ('column', 'Column'), ('triangle', 'Triangle')])
+        self.fields['grid'] = forms.ChoiceField(choices=[('box', 'Box')])
         self.fields['chemistry_time_step'] = forms.FloatField(initial=inits["chemistry_time_step"])
         self.fields['output_time_step'] = forms.FloatField(initial=inits["output_time_step"])
         self.fields['simulation_length'] = forms.FloatField(initial=inits["simulation_length"])
