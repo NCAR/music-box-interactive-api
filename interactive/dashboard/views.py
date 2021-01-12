61a771cad4aba4327109bdad86e4ee4d4ce94802
s-omas <49887549+s-omas@users.noreply.github.com>
2021-01-12 15:06:37 -0700

    plot fixes

--- interactive/dashboard/views.py
+++ interactive/dashboard/views.py
@@ -13,7 +13,7 @@ from django.conf import settings
 import mimetypes
 from django.core.files import File
 from interactive.tools import *
-
+import pandas
 
 def landing_page(request):
     context = {}
@@ -59,7 +59,12 @@ def run_model(request):
 
 
 def visualize(request):
-    context = {}
+    csv_results_path = os.path.join(os.environ['MUSIC_BOX_BUILD_DIR'], "output.csv")
+    csv = pandas.read_csv(csv_results_path)
+    plot_property_list = [x.split('.')[0] for x in csv.columns.tolist()]
+    context = {
+        'plots_list': plot_property_list
+    }
     return render(request, 'plots.html', context)
 
 
