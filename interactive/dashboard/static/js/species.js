d470617ea132eba3ad0f8f0175aaa53337168afa
Matthew Dawson <mattdawson@ucar.edu>
2021-01-14 14:52:15 -0800

    add/remove property functions

--- interactive/dashboard/static/js/species.js
+++ interactive/dashboard/static/js/species.js
@@ -12,6 +12,37 @@ $(document).ready(function(){
     });
   });
 
+  // return html for a property input box
+  function property_input_html(property_name, property_value) {
+    return `
+      <div class="input-group mb-3" property="`+property_name+`">
+        <div class="input-group-prepend">
+          <span class="input-group-text">`+property_name+`</span>
+        </div>
+        <input type="text" class="form-control" placeholder="Property value" value="`+property_value+`">
+        <div class="input-group-append">
+          <button class="btn btn-primary remove-property" property="`+property_name+`">
+            <span class="oi oi-x" toggle="tooltip" aria-hidden="true" title="remove '+property+_name+'"></span>
+          </button>
+        </div>
+      </div>
+    `
+  }
+
+  // remove a property from the species
+  $('.species-detail').on('click', '.btn.remove-property', function(){
+    var property_name = $(this).parents('.input-group').attr('property');
+    $('.species-detail .properties .input-group[property="'+property_name+'"]').remove();
+    $('.new-property .dropdown-item[property="'+property_name+'"]').show();
+  });
+
+  // add property to species
+  $('.species-detail').on('click', '.new-property .dropdown-item', function(){
+    var property_name = $(this).attr('property');
+    $('.species-detail .properties').append(property_input_html(property_name, $(this).attr('default-value')));
+    $('.new-property .dropdown-item[property="'+property_name+'"]').hide();
+  });
+
   // show editable chemical species detail
   $(".species-detail-link").on('click', function(){
     $.ajax({
@@ -28,16 +59,18 @@ $(document).ready(function(){
             <form class="body card-body">
               <div class="form-group properties">
               </div>
-              <hl>
-              <label for="new-property-key" class="font-weight-bold">New property</label>
-              <div class="input-group">
-                <input class="form-control" placeholder="Property name">
-                <div class="input-group-append">
-                  <button class="btn btn-primary btn-ncar-active btn-add">Add</button>
+              <div class="dropdown show">
+                <a href="#" class="btn btn-primary dropdown-toggle" role="button" id="new-property-dropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Add property
+                </a>
+                <div class="dropdown-menu new-property" aria-labelledby="new-property-dropdown">
+                  <a class="dropdown-item" href="#" property="descrition" default-value="">description</a>
+                  <a class="dropdown-item" href="#" property="absolute tolerance" default-value="1e-12">absolute tolerance</a>
+                  <a class="dropdown-item" href="#" property="molecular weight [kg mol-1]" default-value="0">molecular weight</a>
                 </div>
-              </span>
+              </div>
               <div class="container text-center mt-3">
-                <button class="btn btn-primary btn-ncar-active btn-save">Save</button>
+                <button class="btn btn-primary btn-save">Save</button>
                 <button class="btn btn-secondary btn-cancel">Cancel</button>
               </div>
             </form>
@@ -45,19 +78,8 @@ $(document).ready(function(){
         `);
         for (var key of Object.keys(response)) {
           if (key == "name" || key == "type") continue;
-          $('.species-detail .properties').append(`
-            <div class="input-group mb-3">
-              <div class="input-group-prepend">
-                <span class="input-group-text">`+key+`</span>
-              </div>
-              <input type="text" class="form-control" property="`+key+`" placeholder="Property value" value="`+response[key]+`">
-              <div class="input-group-append">
-                <button class="btn btn-primary btn-ncar-active" property="`+key+`">
-                  <span class="oi oi-x" toggle="tooltip" aria-hidden="true" title="remove `+key+`"></span>
-                </button>
-              </div>
-            </div>
-          `);
+          $('.new-property .dropdown-item[property="'+key+'"]').hide();
+          $('.species-detail .properties').append(property_input_html(key, response[key]));
         }
       }
     });
