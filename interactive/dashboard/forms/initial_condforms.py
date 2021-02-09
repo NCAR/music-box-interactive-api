39997cbd9a321b35f1237f6a0e1fab905f65936b
Matthew Dawson <mattdawson@ucar.edu>
2021-02-09 13:18:38 -0800

    finish example 3

--- interactive/dashboard/forms/initial_condforms.py
+++ interactive/dashboard/forms/initial_condforms.py
@@ -11,8 +11,8 @@ class InitialConditionsForm(forms.Form):
         inits = ini_cond_setup()
         self.fields['temperature.label'] = forms.ChoiceField(choices=[('temperature', 'temperature')], widget=forms.Select(attrs={'savebutton': 'initialsSave', 'class': 'form-control'}))
         self.fields['temperature.init'] = forms.FloatField(initial=inits["values"]["temperature"], widget=forms.TextInput(attrs={'savebutton': 'initialsSave', 'class': 'form-control'}))
-        self.fields['temperature.units'] = forms.ChoiceField(initial=inits["units"]["temperature"], choices=[('K', 'K'), ('C', 'C')], widget=forms.Select(attrs={'savebutton': 'initialsSave', 'class': 'form-control'}))
+        self.fields['temperature.units'] = forms.ChoiceField(initial=inits["units"]["temperature"], choices=[('K', 'K'), ('C', 'C'), ('F', 'F')], widget=forms.Select(attrs={'savebutton': 'initialsSave', 'class': 'form-control'}))
         self.fields['pressure.label'] = forms.ChoiceField(choices=[('pressure', 'pressure')], widget=forms.Select(attrs={'savebutton': 'initialsSave', 'class': 'form-control'}))
         self.fields['pressure.init'] = forms.FloatField(initial=inits["values"]["pressure"], widget=forms.TextInput(attrs={'savebutton': 'initialsSave', 'class': 'form-control'}))
-        self.fields['pressure.units'] = forms.ChoiceField(initial=inits["units"]["pressure"], choices=[('atm', 'atm'), ('kPa', 'kPa'), ('bar', 'Bar')], widget=forms.Select(attrs={'savebutton': 'initialsSave', 'class': 'form-control'}))
+        self.fields['pressure.units'] = forms.ChoiceField(initial=inits["units"]["pressure"], choices=[('Pa', 'Pa'), ('atm', 'atm'), ('bar', 'bar'), ('kPa', 'kPa'), ('hPa', 'hPa'), ('mbar', 'mbar')], widget=forms.Select(attrs={'savebutton': 'initialsSave', 'class': 'form-control'}))
 
