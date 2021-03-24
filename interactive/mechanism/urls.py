e217587f7213041e7400b8144698d5eb0d190e88
s-omas <49887549+s-omas@users.noreply.github.com>
2021-03-24 10:48:35 -0600

    urls cleanup

--- interactive/mechanism/urls.py
+++ interactive/mechanism/urls.py
@@ -2,16 +2,21 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.species_home_handler),
-    path('conditions-species-list', views.conditions_species_list_handler),
-    path('reactions', views.reactions_home_handler),
-    path('reaction-detail', views.reaction_detail_handler),
-    path('reaction-musica-names-list', views.reaction_musica_names_list_handler),
-    path('reaction-type-schema', views.reaction_type_schema_handler),
-    path('reaction-remove', views.reaction_remove_handler),
-    path('reaction-save', views.reaction_save_handler),
-    path('species', views.species_home_handler),
-    path('species-detail', views.species_detail_handler),
-    path('species-remove', views.species_remove_handler),
-    path('species-save', views.species_save_handler)
+    # page rendering paths:
+    path('', views.species_home_handler),                                         # renders the chemical species page
+    path('reactions', views.reactions_home_handler),                              # renders the reactions page
+    path('species', views.species_home_handler),                                  # renders the chemical species page
+    # -----------------------------------
+    # data returning paths:
+    path('conditions-species-list', views.conditions_species_list_handler),       # returns a list of species
+    path('reaction-detail', views.reaction_detail_handler),                       # returns a json object for a reaction from the mechanism
+    path('reaction-musica-names-list', views.reaction_musica_names_list_handler), # returns the set of reactions with MUSICA names
+    path('reaction-type-schema', views.reaction_type_schema_handler),             # returns the schema for a reaction type
+    path('species-detail', views.species_detail_handler),                         # returns a json object for a chemical species
+    # -----------------------------------
+    #editing paths:
+    path('reaction-remove', views.reaction_remove_handler),                       # removes a reaction
+    path('reaction-save', views.reaction_save_handler),                           # saves a reaction
+    path('species-remove', views.species_remove_handler),                         # removes a chemical species
+    path('species-save', views.species_save_handler)                              # saves a chemical species
     ]
--- interactive/model_driver/urls.py
+++ interactive/model_driver/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path('run', views.run, name='run'),
     path('check', views.check),
-    path('configure/model/run', views.run),
     path('download', views.download, name="download"),
     path('check-load', views.check_load)
 ]
