33df54aa1d0d6145dfdb52e684a8b3f56dd04695
jwoody02 <jowo4949@colorado.edu>
2022-07-29 15:39:38 -0600

    made first version of github actions file

--- django-unit-tests.py
+++ django-unit-tests.py
@@ -1,9 +1,13 @@
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+
+import django
+django.setup()
 from rest_framework.test import APIRequestFactory
 from rest_framework.test import APITestCase
 from rest_framework.test import APIClient
 from rest_framework.test import force_authenticate
 from rest_framework.test import RequestsClient
-import os
 class APITestCase(TestCase):
 
     def test_config_files_checksums(self):
