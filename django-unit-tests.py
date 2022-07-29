41f70b05325916b4f1e19312226d0937afb28142
jwoody02 <jowo4949@colorado.edu>
2022-07-29 15:36:59 -0600

    made first version of github actions file

---
+++ django-unit-tests.py
@@ -0,0 +1,13 @@
+from rest_framework.test import APIRequestFactory
+from rest_framework.test import APITestCase
+from rest_framework.test import APIClient
+from rest_framework.test import force_authenticate
+from rest_framework.test import RequestsClient
+import os
+class APITestCase(TestCase):
+
+    def test_config_files_checksums(self):
+        """check checksums for config files"""
+        # first lets check that all example files have the right checksums
+        config_dir = os.path.join(os.path.dirname(__file__), 'interactive/dashboard/static/examples')
+        self.assertEqual(config_dir, config_dir)
