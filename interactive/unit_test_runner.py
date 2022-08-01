478df626e508121e2336c09dcfe39469e549216e
jwoody02 <jowo4949@colorado.edu>
2022-08-01 12:56:27 -0600

    added custom unit test runner

--- interactive/unit_test_runner.py
+++ interactive/unit_test_runner.py
@@ -2,13 +2,16 @@ import os
 import json
 from rest_framework import status
 from django.test import TestCase, Client
+from django.test import TransactionTestCase
+from django.test.runner import DiscoverRunner
+from unittest.suite import TestSuite
 
 # initialize the APIClient app
 client = Client()
 
-class UnitTestRunner(TestCase):
+class UnitTestRunner(DiscoverRunner):
 
-    def test_config_files_checksums(self):
+    def test_config_files_checksums(self, **kwargs):
         """check checksums for config files"""
         # first lets check that all example files have the right checksums
         config_dir = os.path.join(os.path.dirname(__file__), 'interactive/dashboard/static/examples')
