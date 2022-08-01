0e2d69a0630321764cd208ff24ebd92d8af44651
jwoody02 <jowo4949@colorado.edu>
2022-08-01 16:10:29 -0600

    more checksum stuff

--- interactive/unit_testing/unit_test_runner.py
+++ interactive/unit_testing/unit_test_runner.py
@@ -22,29 +22,28 @@ class UnitTestRunner(unittest.TestCase):
         # first lets check that all example files have the right checksums
         
         examples_path = os.path.join(base_dir, 'dashboard/static/examples')
-        print("* testing config files checksums in: %s" % examples_path)
+        print("Test Case #1: check file checksums in directory: %s" % examples_path)
 
         # get check sum from example_1
         example_1_path = os.path.join(examples_path, 'example_1')
         example_1_checksum = calculate_checksum(example_1_path)
-        print("* got checksum for "+str(example_1_path)+": "+str(example_1_checksum))
         example_1_expected_checksum = "17158dd0c38c409e58fa8020eb049381" # b3f3a80960f3826896d6f1f116e5f604
         self.assertEqual(example_1_checksum, example_1_expected_checksum)
-        print("|_ example_1 checksum good ✓")
+        print("\t - example_1 checksum good ✓")
 
         # get check sum from example_2
         example_2_path = os.path.join(examples_path, 'example_2')
         example_2_checksum = calculate_checksum(example_2_path)
         example_2_expected_checksum = "e0b89901696a3779ad6b910e24eaeb4c" # e659ad1a567c09dc2c16e32a8f475748
         self.assertEqual(example_2_checksum, example_2_expected_checksum)
-        print("|_ example_2 checksum good ✓")
+        print("\t - example_2 checksum good ✓")
 
         # get check sum from example_3
         example_3_path = os.path.join(examples_path, 'example_3')
         example_3_checksum = calculate_checksum(example_3_path)
-        example_3_expected_checksum = "47ae9f6212cddb7a079119a57aceac9b"
+        example_3_expected_checksum = "f25600931179b57fccdee625e2983d7b" # 47ae9f6212cddb7a079119a57aceac9b
         self.assertEqual(example_3_checksum, example_3_expected_checksum)
-        print("|_ example_3 checksum good ✓")  
+        print("\t - example_3 checksum good ✓")  
 # stop all processes (including running django server)
 def stopAllProcesses():
     print("* stopping all processes")
