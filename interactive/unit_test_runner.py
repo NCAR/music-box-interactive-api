2e9f2c35493f84789be38a6eb27239f866dcf86e
jwoody02 <jowo4949@colorado.edu>
2022-08-01 14:21:01 -0600

    new unittesting strategy

--- interactive/unit_test_runner.py
+++ interactive/unit_test_runner.py
@@ -10,9 +10,9 @@ import subprocess
 # initialize the APIClient app
 client = Client()
 
-class UnitTestRunner(DiscoverRunner):
+class UnitTestRunner(TestCase):
 
-    def test_config_files_checksums(self, **kwargs):
+    def test_config_files_checksums(self):
         """check checksums for config files"""
         # first lets check that all example files have the right checksums
         config_dir = os.path.join(os.path.dirname(__file__), 'interactive/dashboard/static/examples')
