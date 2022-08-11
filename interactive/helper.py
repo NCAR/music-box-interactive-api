e20840c5e8b1e5dd41e99dc6f197c64c02083c80
Jordan Wood <89751870+jwoody02@users.noreply.github.com>
2022-08-11 14:36:11 -0600

    pep8

--- interactive/helper.py
+++ interactive/helper.py
@@ -13,23 +13,24 @@ import logging
 import hashlib
 
 '''
-For the given path, get the List of all files in the directory tree 
+For the given path, get the List of all files in the directory tree
 '''
+
+
 def get_list_of_files(dirName):
-    # create a list of file and sub directories 
-    # names in the given directory 
+    # create a list of file and sub directories
+    # names in the given directory
     listOfFile = os.listdir(dirName)
     allFiles = list()
     # Iterate over all the entries
     for entry in listOfFile:
         # Create full path
         fullPath = os.path.join(dirName, entry)
-        # If entry is a directory then get the list of files in this directory 
+        # If entry is a directory then get the list of files in this directory
         if os.path.isdir(fullPath):
             allFiles = allFiles + get_list_of_files(fullPath)
         else:
-            allFiles.append(fullPath)
-                
+            allFiles.append(fullPath)     
     return allFiles
 
 
