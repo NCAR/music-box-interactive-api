f585957148add9ebc1006427efdae24a3078a2e7
jwoody02 <jowo4949@colorado.edu>
2022-08-12 15:38:30 -0600

    added rabbitmq support

---
+++ interactive/model_driver/rabbit_mq_model_status_listener.py
@@ -0,0 +1,91 @@
+# listener to be hosted on main API server
+# listens for messages from rabbitmq that indicate finished models
+
+import logging
+import pika
+import sys
+import os
+import json
+from session_model_runner import SessionModelRunner
+from django.db import connection
+
+RABBIT_HOST = os.environ["rabbit-mq-host"]
+RABBIT_PORT = int(os.environ["rabbit-mq-port"])
+
+logging.basicConfig(filename='logs.log', filemode='w', format='%(asctime)s - %(message)s', level=logging.INFO)
+logging.basicConfig(format='%(asctime)s - [DEBUG] %(message)s', level=logging.DEBUG)
+logging.basicConfig(filename='errors.log', filemode='w', format='%(asctime)s - [ERROR!!] %(message)s', level=logging.ERROR)
+# disable propagation
+# logging.getLogger("pika").propagate = False
+
+def main():
+    connection = pika.BlockingConnection(pika.ConnectionParameters(RABBIT_HOST, RABBIT_PORT))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='model_finished_queue')
+
+    def run_model_finished_callback(ch, method, properties, body):
+        logging.info('received model_finished message -- extracting results')
+        # TODO: once we have seperated the rabbit_mq_model_runner server from the api server, we would move files
+        decoded_body = json.loads(body.decode())
+        if type(decoded_body) is dict:
+            logging.info('decoded_body is properly formatted as a dict')
+
+            session_id = decoded_body['session_id'] # get session_id from message
+            model_checksum = decoded_body['model_checksum'] # checksum of config files
+            results_name = decoded_body['results_name'] # name for results file
+            results_binary_data = decoded_body['results_binary_data'] # binary data of csv results
+
+            # step 1: save results to file
+            logging.info('saving results to file')
+            results_file_path = os.path.join('/build/'+session_id, results_name)
+            with open(results_file_path, 'wb') as f:
+                f.write(results_binary_data)
+            logging.info('results saved to file')
+
+            # step 2: save checksum to database
+            logging.info('saving checksum to database')
+            
+            cursor = connection.cursor()
+            cursor.execute("UPDATE config_checksums SET model_checksum = %s WHERE id = %s", [model_checksum, session_id])
+            connection.commit()
+            logging.info('checksum saved to database ('+str(model_checksum)+')')
+        else:
+            logging.info('decoded_body is not properly formatted as a dict')
+            # return error?
+    channel.basic_consume(queue='model_finished_queue', on_message_callback=run_model_finished_callback, auto_ack=True)
+
+    print(' [*] Waiting for model_finished_queue messages. To exit press CTRL+C')
+    channel.start_consuming()
+
+
+# checks server by trying to connect
+def check_for_rabbit_mq(host, port):
+    """
+    Checks if RabbitMQ server is running.
+    """
+    try:
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host, port))
+        if connection.is_open:
+            connection.close()
+            return True
+        else:
+            connection.close()
+            return False
+    except:
+        return False
+
+
+if __name__ == '__main__':
+    try:
+        if check_for_rabbit_mq(RABBIT_HOST, RABBIT_PORT):
+            main()
+        else:
+            print('[ERR!] RabbitMQ server is not running. Exiting...')
+            sys.exit(1)
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os.exit(0)
--- interactive/model_driver/session_model_runner.py
+++ interactive/model_driver/session_model_runner.py
@@ -12,19 +12,55 @@ import mimetypes
 import pandas as pd
 import time
 import shutil
-from .run_setup import setup_run
+# from .run_setup import setup_run
 # from .run_logging import *
 from datetime import datetime
 from mechanism.reactions import reactions_are_valid
 from interactive.tools import *
 from pathlib import Path
+from django.conf import settings
 import logging
 import hashlib
-logging.basicConfig(filename='logs.log', filemode='w', format='%(asctime)s - %(message)s', level=logging.INFO)
-logging.basicConfig(format='%(asctime)s - [DEBUG] %(message)s', level=logging.DEBUG)
-logging.basicConfig(filename='errors.log', filemode='w', format='%(asctime)s - [ERROR!!] %(message)s', level=logging.ERROR)
+import pika
+import json
+from update_environment_variables import update_environment_variables
+update_environment_variables()
+RABBIT_HOST = os.environ["rabbit-mq-host"]
+RABBIT_PORT = int(os.environ["rabbit-mq-port"])
 
+# BASE_DIR = '/music-box-interactive/interactive'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'interactive.settings')
+BASE_DIR = settings.BASE_DIR
 
+# disable propagation
+logging.getLogger("pika").propagate = False
+
+# checks server by trying to connect
+def check_for_rabbit_mq(host, port):
+    """
+    Checks if RabbitMQ server is running.
+    """
+    try:
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host, port))
+        if connection.is_open:
+            connection.close()
+            return True
+        else:
+            connection.close()
+            return False
+    except:
+        return False
+# add session_id to queue model_finished
+def add_status_to_queue(session_id, host, port, status):
+    """
+    Adds session_id to queue model_finished.
+    """
+    message = {'session_id': session_id, "model_status": status}
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host, port))
+    channel = connection.channel()
+    channel.queue_declare(queue='model_finished_queue')
+    channel.basic_publish(exchange='', routing_key='model_finished_queue', body=json.dumps(message))
+    connection.close()
 class SessionModelRunner():
     def __init__(self, session_id):
         self.setPathsForSessionID(session_id)
@@ -33,33 +69,33 @@ class SessionModelRunner():
         mb_dir = os.path.join(os.environ['MUSIC_BOX_BUILD_DIR'])
         interface_solo = False
     else:
-        logging.debug(os.environ)
+        print(os.environ)
         mb_dir = ''
         interface_solo = True
 
     out_path = os.path.join(mb_dir, 'output.csv')
     error_path = os.path.join(mb_dir, 'error.json')
     copy_path = os.path.join(
-        settings.BASE_DIR, 'dashboard/static/past_run/past.csv')
+        BASE_DIR, 'dashboard/static/past_run/past.csv')
     config_path = os.path.join(
-        settings.BASE_DIR, "dashboard/static/config/my_config.json")
+        BASE_DIR, "dashboard/static/config/my_config.json")
     old_path = os.path.join(
-        settings.BASE_DIR, "dashboard/static/config/old_config.json")
+        BASE_DIR, "dashboard/static/config/old_config.json")
     complete_path = os.path.join(mb_dir, 'MODEL_RUN_COMPLETE')
 
     config_dest = os.path.join(
-        settings.BASE_DIR, 'dashboard/static/past_run/config.json')
+        BASE_DIR, 'dashboard/static/past_run/config.json')
 
     config_folder_path = os.path.join(
-        settings.BASE_DIR, "dashboard/static/config")
-    log_path = os.path.join(settings.BASE_DIR, 'dashboard/static/log/log')
+        BASE_DIR, "dashboard/static/config")
+    log_path = os.path.join(BASE_DIR, 'dashboard/static/log/log')
     camp_folder_path = os.path.join(
-        settings.BASE_DIR, "dashboard/static/config/camp_data")
+        BASE_DIR, "dashboard/static/config/camp_data")
 
     reactions_path = os.path.join(
-        settings.BASE_DIR, "dashboard/static/config/camp_data/reactions.json")
+        BASE_DIR, "dashboard/static/config/camp_data/reactions.json")
     species_path = os.path.join(
-        settings.BASE_DIR, "dashboard/static/config/camp_data/species.json")
+        BASE_DIR, "dashboard/static/config/camp_data/species.json")
 
     sessionid = ""
 
@@ -85,24 +121,24 @@ class SessionModelRunner():
         self.out_path = os.path.join(self.mb_dir, 'output.csv')
         self.error_path = os.path.join(self.mb_dir, 'error.json')
         self.copy_path = os.path.join(
-            settings.BASE_DIR, 'past_run/'+session_id+'/past.csv')
+            BASE_DIR, 'past_run/'+session_id+'/past.csv')
         self.config_path = os.path.join(
-            settings.BASE_DIR, "configs/"+session_id+"/my_config.json")
+            BASE_DIR, "configs/"+session_id+"/my_config.json")
         self.old_path = os.path.join(
-            settings.BASE_DIR, "configs/"+session_id+"/old_config.json")
+            BASE_DIR, "configs/"+session_id+"/old_config.json")
         self.complete_path = os.path.join(self.mb_dir, 'MODEL_RUN_COMPLETE')
         self.config_dest = os.path.join(
-            settings.BASE_DIR, 'past_run/'+session_id+'/config.json')
+            BASE_DIR, 'past_run/'+session_id+'/config.json')
         self.config_folder_path = os.path.join(
-            settings.BASE_DIR, "configs/"+session_id)
-        self.log_path = os.path.join(settings.BASE_DIR, 'logs/'+session_id)
+            BASE_DIR, "configs/"+session_id)
+        self.log_path = os.path.join(BASE_DIR, 'logs/'+session_id)
         self.camp_folder_path = os.path.join(
-            settings.BASE_DIR, "configs/"+session_id+"/camp_data")
+            BASE_DIR, "configs/"+session_id+"/camp_data")
         reac = "/camp_data/reactions.json"
         self.reactions_path = os.path.join(
-            settings.BASE_DIR, "configs/"+session_id+reac)
+            BASE_DIR, "configs/"+session_id+reac)
         self.species_path = os.path.join(
-            settings.BASE_DIR, "configs/"+session_id+"/camp_data/species.json")
+            BASE_DIR, "configs/"+session_id+"/camp_data/species.json")
         self.sessionid = session_id
 
     def add_integrated_rates(self):
@@ -204,7 +240,7 @@ class SessionModelRunner():
         if not os.path.exists(self.log_path):
             os.makedirs(self.log_path)
         originalPath = os.path.join(
-            settings.BASE_DIR, 'dashboard/static/log/log_config.json')
+            BASE_DIR, 'dashboard/static/log/log_config.json')
         self.copyAFile(originalPath, os.path.join(
             self.log_path, "log_config.json"))
         time.sleep(0.1)
@@ -216,7 +252,7 @@ class SessionModelRunner():
                            self.mb_dir+'/mb_configuration', f),
                            os.path.join(self.mb_dir, f))
         checksum = self.calculate_checksum()
-        logging.info("calculated checksum: " + checksum)
+        print("calculated checksum: ", checksum)
         logging.info("running model from base directory: " + self.mb_dir)
         process = subprocess.Popen(
             [r'../music_box', r'./mb_configuration/my_config.json'],
@@ -228,7 +264,36 @@ class SessionModelRunner():
             json.dump(species_data, z)
 
         return {'model_running': True}
+     # helper function getting all files for user session
+    '''
+    For the given path, get the List of all files in the directory tree 
+    '''
+    def getListOfFiles(self, dirName):
+        # create a list of file and sub directories 
+        # names in the given directory 
+        listOfFile = os.listdir(dirName)
+        allFiles = list()
+        # Iterate over all the entries
+        for entry in listOfFile:
+            # Create full path
+            fullPath = os.path.join(dirName, entry)
+            # If entry is a directory then get the list of files in this directory 
+            if os.path.isdir(fullPath):
+                allFiles = allFiles + self.getListOfFiles(fullPath)
+            else:
+                allFiles.append(fullPath)
+                    
+        return allFiles
 
+
+    # calculate checksum for config/model so we can check if it's run before
+    def calculate_checksum(self):
+        filenames = self.getListOfFiles(self.mb_dir)
+        hash = hashlib.md5()
+        for fn in filenames:
+            if os.path.isfile(fn):
+                hash.update(open(fn, "rb").read())
+        return hash.hexdigest()
     # copy inital config file on first model run
     def setup_config_check(self):
         self.copyAFile(self.config_path, self.old_path)
@@ -272,6 +337,8 @@ class SessionModelRunner():
                         response_message.update({'spec_ID': key + '.Formula'})
             response_message.update({'e_code': errorfile['code']})
             response_message.update({'e_message': errorfile['message']})
+        # if check_for_rabbit_mq(RABBIT_HOST, RABBIT_PORT) and (status == 'done' or status == 'error'):
+        #     add_status_to_queue(self.sessionid, RABBIT_HOST, RABBIT_PORT, response_message)
         return JsonResponse(response_message)
 
     def check_load(self, request):
@@ -290,9 +357,11 @@ class SessionModelRunner():
                 status = 'done'
 
         response_message.update({'status': status, 'session_id': request.session.session_key})
+        # if check_for_rabbit_mq(RABBIT_HOST, RABBIT_PORT) and (status == 'done' or status == 'error'):
+        #     add_status_to_queue(self.sessionid, RABBIT_HOST, RABBIT_PORT, response_message)
         return JsonResponse(response_message)
-
-    def run(self, request):
+    
+    def run(self):
         logging.info("running...")
         run = self.setup_run()
         self.save_run()
@@ -378,34 +447,3 @@ class SessionModelRunner():
                                       'log_config.json'), lc)
         logging.info('log cleared')
     
-
-    # helper function getting all files for user session
-    '''
-    For the given path, get the List of all files in the directory tree 
-    '''
-    def getListOfFiles(self, dirName):
-        # create a list of file and sub directories 
-        # names in the given directory 
-        listOfFile = os.listdir(dirName)
-        allFiles = list()
-        # Iterate over all the entries
-        for entry in listOfFile:
-            # Create full path
-            fullPath = os.path.join(dirName, entry)
-            # If entry is a directory then get the list of files in this directory 
-            if os.path.isdir(fullPath):
-                allFiles = allFiles + self.getListOfFiles(fullPath)
-            else:
-                allFiles.append(fullPath)
-                    
-        return allFiles
-
-
-    # calculate checksum for config/model so we can check if it's run before
-    def calculate_checksum(self):
-        filenames = self.getListOfFiles(self.mb_dir)
-        hash = hashlib.md5()
-        for fn in filenames:
-            if os.path.isfile(fn):
-                hash.update(open(fn, "rb").read())
-        return hash.hexdigest()
