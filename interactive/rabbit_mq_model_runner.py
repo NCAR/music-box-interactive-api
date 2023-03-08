c2b9721222156bf50119767c4112c5496ddc5d91
Kyle Shores <kyle.shores44@gmail.com>
2023-03-08 17:03:21 -0600

    trying to get runner to connect to rabbit

--- interactive/rabbit_mq_model_runner.py
+++ interactive/rabbit_mq_model_runner.py
@@ -36,7 +36,8 @@ rabbit_port = int(os.environ['rabbit-mq-port'])
 logging.getLogger("pika").propagate = False
 def callback(session_id, config_files_dict, future):
     con_params = pika.ConnectionParameters(rabbit_host, rabbit_port)
-    connection = pika.BlockingConnection(con_params)
+    # connection = pika.BlockingConnection(con_params)
+    connection = pika.BlockingConnection(pika.URLParameters("amqp://guest:guest@rabbitmq:5672/"))
 
     if future.exception() is not None:
         logging.info("["+session_id+"] Got exception: %s" % future.exception())
