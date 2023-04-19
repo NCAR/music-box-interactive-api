7a7107e05caa56ca8b2d639802500302a393a769
Kyle Shores <kyle.shores44@gmail.com>
2023-04-19 11:38:21 -0500

    updating checksum

--- interactive/test_examples.py
+++ interactive/test_examples.py
@@ -70,7 +70,7 @@ class UnitTestRunner(unittest.TestCase):
         # get check sum from CHAPMAN
         CHAPMAN_path = os.path.join(examples_path, 'CHAPMAN')
         CHAPMAN_checksum = calculate_checksum(CHAPMAN_path)
-        CHAPMAN_expected_checksum = "f0a73918971c2824fa8cc1d4bb88ea0b"
+        CHAPMAN_expected_checksum = "91c1cf2d5a317ddeac48b5ed30c8fa90"
         self.assertEqual(CHAPMAN_checksum, CHAPMAN_expected_checksum)
         print("\t - CHAPMAN checksum good ✓")
 
