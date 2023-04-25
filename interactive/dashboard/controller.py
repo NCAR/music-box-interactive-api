f5732ee069d054a0bbac43f5d50d3639256134ea
Kyle Shores <kyle.shores44@gmail.com>
2023-04-25 15:23:07 -0500

    returning camp species and reactions

--- interactive/dashboard/controller.py
+++ interactive/dashboard/controller.py
@@ -1,21 +1,32 @@
 from django.conf import settings
 from django.http import Http404
 
+import json
 import logging
 import os
 
 logger = logging.getLogger(__name__)
 
+
 def load_example(example):
-  conditions = {}
-  mechanism = {}
+    conditions = {}
+    mechanism = {}
 
-  example_path = os.path.join(
-      settings.BASE_DIR, 'dashboard/static/examples', example)
+    example_path = os.path.join(
+        settings.BASE_DIR, 'dashboard/static/examples', example)
 
-  files = [os.path.join(dp, f) for dp, _, fn in os.walk(example_path) for f in fn]
-  logger.info(files)
-  if not files:
-      raise Http404("No files in example folder")
-    
-  return conditions, mechanism
+    files = [os.path.join(dp, f)
+             for dp, _, fn in os.walk(example_path) for f in fn]
+    logger.info(files)
+    if not files:
+        raise Http404("No files in example folder")
+
+    for file in files:
+        if 'species.json' in file:
+            with open(file) as contents:
+                mechanism['species'] = json.load(contents)
+        if 'reactions.json' in file:
+            with open(file) as contents:
+                mechanism['reactions'] = json.load(contents)
+
+    return conditions, mechanism
