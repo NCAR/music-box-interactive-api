2e6ce7875ab37cb2f8d9d55928d489d75b3238d2
Kyle Shores <kyle.shores44@gmail.com>
2023-05-16 12:32:10 -0500

    Swagger on acom server (#344)
    * collecting static files and serving on a volume
    
    * removing unused cores header
    
    * adding static files volume
    
    * setting static dir to the one gatsby uses
    
    * removing volume that is not used
    
    * correcting parts of open api
    
    * properly setting base path
    
    ---------
    
    Co-authored-by: Kyle Shores <kshores@ucar.edu>

--- interactive/manage/urls.py
+++ interactive/manage/urls.py
@@ -1,23 +1,33 @@
 from django.urls import path, include
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from drf_yasg.generators import OpenAPISchemaGenerator
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+import os
 
 import manage.api as api
 
 app_name = 'manage'
 
+class SchemaGenerator(OpenAPISchemaGenerator):
+  def get_schema(self, request=None, public=False):
+    schema = super(SchemaGenerator, self).get_schema(request, public)
+    schema.basePath = os.path.join(schema.basePath, 'musicbox/')
+    return schema
+
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="MusicBox Interactive API",
       default_version='v1',
       description="MusicBox interactive API",
       terms_of_service="https://www.google.com/policies/terms/",
       contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
+      license=openapi.License(name="Apache 2.0"),
    ),
    public=True,
    permission_classes=[permissions.AllowAny],
+   generator_class=SchemaGenerator,
 )
 
 urlpatterns = [
