cfcb7f1238bb75ad575e17212e63f5cc8ea631b8
Kyle Shores <kyle.shores44@gmail.com>
2024-09-02 13:30:52 -0500

    Redo changes (#439)
    * redoing changes in #394
    
    * rmeoving status listener
    
    * removing status listener
    
    * more merge resolves
    
    * correcting db volumtes

--- interactive/rabbit_mq_model_runner.py
+++ interactive/rabbit_mq_model_runner.py
@@ -1,3 +1,9 @@
+# these imports must come first # noqa: E402
+import os  # noqa: E402
+import django  # noqa: E402
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'manage.settings')  # noqa: E402
+django.setup()  # noqa: E402
+
 import sys
 import shutil
 import logging
@@ -6,38 +12,39 @@ import functools
 import traceback
 import os
 import time
-from shared.rabbit_mq import consume, rabbit_is_available, publish_message, ConsumerConfig
+from api.controller import get_model_run
+from shared.rabbit_mq import consume, rabbit_is_available, ConsumerConfig
 from shared.configuration_utils import load_configuration, \
     get_working_directory, \
     get_session_path
 from api.run_status import RunStatus
 from multiprocessing import cpu_count
 from concurrent.futures import ThreadPoolExecutor as Pool
+import pandas as pd
 
 from acom_music_box import MusicBox
 
-
-# main model runner interface class for rabbitmq and actual model runner
-# 1) listen to run_queue
-# 2) run model when receive message
-# 3) send message to model_finished_queue when model is finished
-
-# cpu_count() returns number of cores on machine
-# this will run as many threads as there are cores (and will be faster but very cpu intensive)
-# set this number to 1 if you want to run everything in one thread -- if you are running on a server
-# without much cpu power/want to reduce energy usage you should probably
-# set this to 1
-# sets max number of workers to add to pool
 pool = Pool(max_workers=cpu_count())
 
 # disable propagation
 logging.getLogger("pika").propagate = False
 
+def set_model_run_status(session_id, status, error=None, output=None, partmc_output_path=None):
+    model_run = get_model_run(session_id)
+    model_run.status = status
+    model_run.results['error'] = error
+    model_run.results['/output.csv'] = output
+    if partmc_output_path:
+        model_run.results['partmc_output_path'] = partmc_output_path
+    model_run.save()
+    logging.info(f"Model run saved to database for session {session_id}")
+
 
 def music_box_exited_callback(session_id, output_directory, future):
     exception_message = None
-    route_key = RunStatus.DONE.value
-    body = {'session_id': session_id}
+    status = RunStatus.DONE.value
+    output = None
+    error = None
 
     if future.exception() is not None:
         exception_message = ''.join(
@@ -46,7 +53,7 @@ def music_box_exited_callback(session_id, output_directory, future):
                 future.exception(),
                 future.exception().__traceback__))
         logging.error(f"[{session_id}] MusicBox finished with exception: {exception_message}")
-        route_key = RunStatus.ERROR.value
+        status = RunStatus.ERROR.value
     else:
         logging.info(f"[{session_id}] MusicBox finished normally")
 
@@ -55,48 +62,53 @@ def music_box_exited_callback(session_id, output_directory, future):
         if len(output_files) == 0:
             logging.info(f"[{session_id}] No output files found")
             exception_message = "No output files found"
-            route_key = RunStatus.ERROR.value
+            status = RunStatus.ERROR.value
 
         csv_path = os.path.join(output_directory, 'output.csv')
-
-        if os.path.exists(csv_path):
-            with open(csv_path, 'r') as f:
-                body["output.csv"] = f.read()
+        output = pd.read_csv(csv_path).to_csv(index=False)
 
         # remove all files to save space
         shutil.rmtree(output_directory)
 
     if exception_message is not None:
-        # add exception message to body
-        body["error.json"] = json.dumps({'message': exception_message})
-
-    logging.info(f"[{session_id}] Sending message with route key: {route_key}")
-    publish_message(route_key=route_key, message=body)
+        error = json.dumps({'message': exception_message})
+    
+    set_model_run_status(session_id, status, error=error, output=output)
 
 
 def partmc_exited_callback(session_id, future):
-    body = {'session_id': session_id}
-    route_key = RunStatus.DONE.value
+    exception_message = None
+    status = RunStatus.DONE.value
+    error = None
+    partmc_output_path = None
+
+    status = RunStatus.DONE.value
     if future.exception() is not None:
-        body['error.json'] = json.dumps(
-            {'message': str(future.exception())})
-        route_key = RunStatus.ERROR.value
-        logging.error(f"[{session_id}] Got exception: %s" % future.exception())
+        exception_message = ''.join(
+            traceback.format_exception(
+                None,
+                future.exception(),
+                future.exception().__traceback__))
+        logging.error(f"[{session_id}] MusicBox finished with exception: {exception_message}")
+        status = RunStatus.ERROR.value
     else:
         logging.info(f"[{session_id}] PartMC Model finished.")
 
         output_directory = f"/partmc/partmc-volume/{session_id}"
-        body['partmc_output_path'] = f"/music-box-interactive/interactive/partmc-volume/{session_id}"
+        partmc_output_path = f"/music-box-interactive/interactive/partmc-volume/{session_id}"
 
         # check number of output files in output directory
         output_files = getListOfFiles(output_directory)
         if len(output_files) == 0:
             logging.info(f"[{session_id}] No output files found")
             shutil.rmtree(output_directory)
-            route_key = RunStatus.ERROR.value
-            body['error.json'] = json.dumps({'message': 'No output files found for PartMC'})
+            status = RunStatus.ERROR.value
+            exception_message = "No output files found"
 
-    publish_message(route_key=route_key, message=body)
+    if exception_message is not None:
+        error = json.dumps({'message': exception_message})
+
+    set_model_run_status(session_id, status, error=error, partmc_output_path=partmc_output_path)
 
 
 def run_request_callback(ch, method, properties, body):
@@ -129,7 +141,7 @@ def run_request_callback(ch, method, properties, body):
     except Exception as e:
         body = {"error.json": json.dumps(
             {'message': str(e)}), "session_id": session_id}
-        publish_message(route_key=RunStatus.ERROR.value, message=body)
+        set_model_run_status(session_id, RunStatus.ERROR.value, error=body)
         logging.exception('Setting up run failed')
 
 
@@ -157,15 +169,11 @@ def run_music_box(session_id):
         )
     )
 
-    body = {"session_id": session_id}
-    publish_message(route_key=RunStatus.RUNNING.value, message=body)
-
+    set_model_run_status(session_id, RunStatus.RUNNING.value)
 
 def run_partmc(session_id):
     from partmc_model.default_partmc import run_pypartmc_model
     os.makedirs(f"/partmc/partmc-volume/{session_id}", exist_ok=True)
-    body = {"session_id": session_id}
-    publish_message(route_key=RunStatus.RUNNING.value, message=body)
     future = pool.submit(
         run_pypartmc_model,
         session_id
@@ -175,7 +183,7 @@ def run_partmc(session_id):
             partmc_exited_callback,
             session_id,
         ))
-
+    set_model_run_status(session_id, RunStatus.RUNNING.value)
 
 def main():
     consume(consumer_configs=[
